40ec418757ff3385a4a0b469e32141d00c5405a1
Owner <vitycity79@gmail.com>
2023-07-11 16:59:23 +0300

    innit commit

--- src/ClientsLIst.java
+++ src/ClientsLIst.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class ClientsLIst {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class ClientsLIst {
+    private final String clientsList = "clients.txt";
+    private final String file;
+
+    public ClientsLIst() {
+//        String filePath = "src";
+        this.file = new File("").getAbsolutePath() + "/" + clientsList;
+    }
+
+    public String getClientsList() {
+        return clientsList;
+    }
+    public String getFile() {
+        return file;
+    }
+
+    public String uniqueWords (FileReader bookName) throws IOException {
+        ArrayList <String> unique = new ArrayList<>();
+        int c;
+        StringBuilder wordBuilder = new StringBuilder();
+        while ((c = bookName.read()) != -1) {
+            wordBuilder.append((char) c);
+            if (c == 32) {
+                unique.add(wordBuilder.toString());
+                wordBuilder = new StringBuilder();
+            }
+        }
+        HashMap<String, Integer> mostPopular = new HashMap<>();
+        for (String word : unique) {
+            if (!mostPopular.isEmpty()) {
+                if (mostPopular.containsKey(word)) {
+                    mostPopular.replace(word, mostPopular.get(word), mostPopular.get(word) + 1);
+                }
+                else mostPopular.put(word, 1);
+            }
+            else mostPopular.put(word, 1);
+        }
+        LinkedHashMap<String, Integer> mostPopularOrder = new LinkedHashMap<>();
+        mostPopular.entrySet().stream()
+                .filter(v -> v.getValue() > 1)
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(w -> mostPopularOrder.put(w.getKey(), w.getValue()));
+
+        return mostPopularOrder.toString();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.awt.print.Book;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ClientsLIst clientsLIst = new ClientsLIst();
+
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(clientsLIst.getFile());
+            System.out.println(clientsLIst.uniqueWords(fileReader));
+            fileReader.close();
+        } catch (IOException e) {
+            System.err.println("No such file was found. Please, enter the correct title");
+        }
+
+        String statisticsFile = clientsLIst.getFile() + "_statistic.txt";
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(statisticsFile, true);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            fileReader = new FileReader(clientsLIst.getFile());
+            fileWriter.write(clientsLIst.uniqueWords(fileReader));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
